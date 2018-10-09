6baffa749389be5dd8a87b673ba488d7511207ca
tjlr50 <tjlr50@gmail.com>
2018-10-08 19:20:01 -0500

    Update travis_Test

--- gm_analytics/handlers.py
+++ gm_analytics/handlers.py
@@ -1,6 +1,16 @@
+import logging
+
 def get_user_info(username=None):
+    logging.info("punto user info")
     return {"user_info": username}
 
+
 def get_commits_info(username=None, time_range=None):
+    logging.info("punto de commit_info")
     return [{'commits_count': 10, 'yyyymmdd_date': '20180101'},
             {'commits_count': 10, 'yyyymmdd_date': '20180102'}]
+
+
+def get_health():
+    logging.info("punto health")
+    return {"message": "ok"}
